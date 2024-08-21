0b165c9850fbcb562aeb1e297ccbbd1d996eb2cb
JanJangura <111806379+JanJangura@users.noreply.github.com>
2024-08-21 14:44:14 -0400

    Start of Level Designing

--- Source/BlasterGame/GameMode/LobbyGameMode.cpp
+++ Source/BlasterGame/GameMode/LobbyGameMode.cpp
@@ -16,7 +16,7 @@ void ALobbyGameMode::PostLogin(APlayerController* NewPlayer)
 	// it has a Num() function that returns the number of elements within the array as an int32.
 	int32 NumberOfPlayers = GameState.Get()->PlayerArray.Num();
 
-	if (NumberOfPlayers == 2) {	// We'll check to see if the amount of players have been met, so we can travel to the actual game when it's met.
+	if (NumberOfPlayers == 1) {	// We'll check to see if the amount of players have been met, so we can travel to the actual game when it's met.
 		// We'll call ServerTravel() so all connecting players from the lobby, can travel to the Actual Game. Remember, ServerTravel() is a function that belongs to UWorld.
 		UWorld* World = GetWorld();
 
@@ -27,7 +27,7 @@ void ALobbyGameMode::PostLogin(APlayerController* NewPlayer)
 			// ServerTravel() takes in an address to travel to. The server only ever exists on the GameMode, so when we call this function, we can travel to the level we want to. 
 			// Then all connected clients will travel to that level. In parameters, ServerTravel() takes an FString, then we can use the syntax "?" to add additional options to 
 			// the address. In this case, the option we need is "listen" because we want to dedicate this BlasterMap to be opened as a listen server for clients to connect to. 
-			World->ServerTravel(FString("/Game/Scenes/BlasterMap?listen")); 
+			World->ServerTravel(FString("/Game/Scenes/BlasterMap?listen")); 			
 			GEngine->AddOnScreenDebugMessage(
 				-1,
 				15.f,
