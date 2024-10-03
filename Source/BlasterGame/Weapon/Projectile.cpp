20844f3c1c3e25a70a3ee1fc6f8fbf762204a66d
JanJangura <111806379+JanJangura@users.noreply.github.com>
2024-10-02 20:40:01 -0400

    Projectiles Collision now Destroyed
    Projectiles are now Replicated and also Destroyed.

--- Source/BlasterGame/Weapon/Projectile.cpp
+++ Source/BlasterGame/Weapon/Projectile.cpp
@@ -4,6 +4,8 @@
 #include "Projectile.h"
 #include "Components/BoxComponent.h"
 #include "GameFramework/ProjectileMovementComponent.h"
+#include "Kismet/GameplayStatics.h"
+#include "Sound/SoundCue.h"
 
 // Sets default values
 AProjectile::AProjectile()
@@ -26,6 +28,16 @@ void AProjectile::BeginPlay()
 {
 	Super::BeginPlay();
 	
+	if (HasAuthority()) {	// The Server can handle on doing the damage
+		CollisionBox->OnComponentHit.AddDynamic(this, &AProjectile::OnHit);	// Within Parameters of Add Dynamic, User Object is this Projectile class, and then the address of the operator.
+	}
+}
+
+// This is how we know we hit something.
+void AProjectile::OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit)
+{
+	// This Destroys our Projectile Actor.
+	Destroy();
 }
 
 void AProjectile::Tick(float DeltaTime)
@@ -34,3 +46,15 @@ void AProjectile::Tick(float DeltaTime)
 
 }
 
+void AProjectile::Destroyed()
+{
+	Super::Destroyed();
+
+	// This is Replicated already from the base class Destroyed, we're just adding on to it.
+	if (ImpactParticles) {
+		UGameplayStatics::SpawnEmitterAtLocation(GetWorld(), ImpactParticles, GetActorTransform());	// Play the Particle at this Actor's Transform (Location)
+	}
+	if (ImpactSound) {
+		UGameplayStatics::PlaySoundAtLocation(this, ImpactSound, GetActorLocation());
+	}
+}
--- Source/BlasterGame/Weapon/Projectile.h
+++ Source/BlasterGame/Weapon/Projectile.h
@@ -14,9 +14,13 @@ class BLASTERGAME_API AProjectile : public AActor
 public:	
 	AProjectile();
 	virtual void Tick(float DeltaTime) override;
+	virtual void Destroyed() override;	// This will also be called on Client and Server as the action of Destroying() propogates on clients.
 
 protected:
 	virtual void BeginPlay() override;
+
+	UFUNCTION()
+	virtual void OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit);
 
 private:
 	UPROPERTY(EditAnywhere)
@@ -24,6 +28,13 @@ private:
 
 	UPROPERTY(VisibleAnywhere)
 	class UProjectileMovementComponent* ProjectileMovementComponent;
+
+	UPROPERTY(EditAnywhere)
+	UParticleSystem* ImpactParticles;
+
+	UPROPERTY(EditAnywhere)
+	class USoundCue* ImpactSound;
+
 public:	
 	
 
