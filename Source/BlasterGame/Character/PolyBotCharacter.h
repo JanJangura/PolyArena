9dc03d44fb8312e902f8911160690bdf665aef1f
JanJangura <111806379+JanJangura@users.noreply.github.com>
2025-02-26 10:41:58 -0500

    PolyBot Character Added

---
+++ Source/BlasterGame/Character/PolyBotCharacter.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BlasterCharacter.h"
+#include "PolyBotCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BLASTERGAME_API APolyBotCharacter : public ABlasterCharacter
+{
+	GENERATED_BODY()
+	
+public:
+	APolyBotCharacter();
+};
